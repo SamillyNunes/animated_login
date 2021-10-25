0e922a4735066cdcf7db9835680a8023f7949baa
Samilly <samillynunes19@gmail.com>
2021-10-25 11:59:49 -0300

    Animating pages and widgets in pages

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,3 +1,4 @@
+import 'package:animated_login/constants.dart';
 import 'package:animated_login/core/app_colors.dart';
 import 'package:animated_login/core/app_vectors.dart';
 import 'package:animated_login/shared/widgets/login_form.dart';
@@ -6,8 +7,15 @@ import 'package:animated_login/shared/widgets/social_buttons.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
   const AuthPage({Key? key}) : super(key: key);
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool _showSignUp = false;
 
   @override
   Widget build(BuildContext context) {
@@ -17,41 +25,53 @@ class AuthPage extends StatelessWidget {
       body: Stack(
         children: [
           // login form
-          Positioned(
+          AnimatedPositioned(
+            duration: defaultDuration,
             width: _size.width * 0.88,
             height: _size.height,
-            left: -_size.width * 0.76,
-            child: Container(
-              color: AppColors.loginBg,
-              child: const LoginForm(),
+            left: _showSignUp ? -_size.width * 0.76 : 0,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _showSignUp = !_showSignUp;
+                });
+              },
+              child: Container(
+                color: AppColors.loginBg,
+                child: const LoginForm(),
+              ),
             ),
           ),
           // sign up button
-          Positioned(
+          AnimatedPositioned(
+            duration: defaultDuration,
             height: _size.height,
             width: _size.width * 0.88,
-            left: _size.width * 0.12,
+            left: _showSignUp ? _size.width * 0.12 : _size.width * 0.88,
             child: Container(
               color: AppColors.signupBg,
               child: const SignUpForm(),
             ),
           ),
-          Positioned(
+          AnimatedPositioned(
+            duration: defaultDuration,
             left: 0,
-            right: _size.width * 0.06,
+            right: _showSignUp ? -_size.width * 0.06 : _size.width * 0.06,
             top: _size.height * 0.1,
             child: CircleAvatar(
               radius: 25,
               backgroundColor: Colors.white60,
               child: SvgPicture.asset(
                 AppVectors.animatedLogo,
+                color: _showSignUp ? AppColors.signupBg : AppColors.loginBg,
               ),
             ),
           ),
-          Positioned(
+          AnimatedPositioned(
+            duration: defaultDuration,
             width: _size.width,
             bottom: _size.height * 0.1,
-            right: _size.width * 0.06,
+            right: _showSignUp ? -_size.width * 0.06 : _size.width * 0.06,
             child: const SocialButtons(),
           ),
         ],
